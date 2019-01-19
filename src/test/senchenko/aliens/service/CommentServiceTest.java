43408b8660071100bcb4fbfd08386b005f2c0620
SenchenkoDV <senchenkodv@gmail.com>
2019-01-19 16:07:02 +0300

    Has been added tests.

--- src/test/senchenko/aliens/service/CommentServiceTest.java
+++ src/test/senchenko/aliens/service/CommentServiceTest.java
@@ -1,6 +1,7 @@
 package test.senchenko.aliens.service;
 
 import com.senchenko.aliens.command.CommandResult;
+import com.senchenko.aliens.controller.FrontServlet;
 import com.senchenko.aliens.controller.RequestContent;
 import com.senchenko.aliens.dao.CommentDao;
 import com.senchenko.aliens.dao.SingletonDaoProvider;
@@ -15,60 +16,86 @@ import com.senchenko.aliens.service.MonsterService;
 import com.senchenko.aliens.service.UserRatingAction;
 import com.senchenko.aliens.validation.CommentValidator;
 import com.senchenko.aliens.validation.UserValidation;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-
 import java.util.HashMap;
-
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
-import static org.testng.Assert.assertEquals;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({TransactionExecutor.class, RequestContent.class, CommentDao.class, CommentValidator.class, UserValidation.class,
-        UserRatingAction.class, CommentService.class, SingletonDaoProvider.class})
+@PrepareForTest({TransactionExecutor.class, RequestContent.class, SingletonDaoProvider.class, CommentDao.class, CommentValidator.class, UserValidation.class,
+        UserRatingAction.class, CommentService.class, FrontServlet.class})
 public class CommentServiceTest {
-    @Test
-    public void addComment() throws Exception {
-        String expectedPage = "/jsp/monster.jsp";
-        CommentService commentService = spy(CommentService.class);
-        TransactionExecutor transactionExecutor = mock(TransactionExecutor.class);
-        CommentDao commentDao = mock(CommentDao.class);
-        UserDao userDao = mock(UserDao.class);
-        RequestContent content = mock(RequestContent.class);
-        MonsterService monsterService = mock(MonsterService.class);
+    String expectedPage;
+    CommentService commentService;
+    TransactionExecutor transactionExecutor;
+    CommentDao commentDao;
+    UserDao userDao;
+    RequestContent content;
+    MonsterService monsterService;
+    HashMap<String, String[]> requestParameters;
+    HashMap<String, Object> sessionAttributes;
+    User user;
+    Monster monster;
+
+    @Before
+    public void setUp() throws Exception {
+        expectedPage = "/jsp/monster.jsp";
+        requestParameters = new HashMap<>();
+        sessionAttributes = new HashMap<>();
+        requestParameters.put("star", new String[]{"1"});
+        requestParameters.put("comment", new String[]{"test"});
+        requestParameters.put("monsterId", new String[]{"1"});
+        user = new User(1, new Role(1, "admin"),
+                1, "test", "test", "test");
+        sessionAttributes.put("user",user);
+        monster = new Monster(-1,  "test",
+                new Race(1, "test"),"test", 0.0,"test");
+        sessionAttributes.put("monster", monster);
+        commentService = new CommentService();
+        content = mock(RequestContent.class);
+        transactionExecutor = mock(TransactionExecutor.class);
+        commentDao = mock(CommentDao.class);
+        userDao = mock(UserDao.class);
+        monsterService = mock(MonsterService.class);
         PowerMockito.mockStatic(CommentValidator.class);
         PowerMockito.mockStatic(UserValidation.class);
         PowerMockito.mockStatic(UserRatingAction.class);
+    }
 
-        HashMap<String, String[]> map1 = new HashMap<>();
-        HashMap<String, Object> map2 = new HashMap<>();
-        map1.put("star", new String[]{"1"});
-        map1.put("comment", new String[]{"test"});
-        map1.put("monsterId", new String[]{"1"});
-        User user = new User(1, new Role(1, "admin"), 1, "test", "test", "test");
-        map2.put("user",user);
-        Monster monster = new Monster(-1,  "test", new Race(1, "test"),"test", 0.0,"test");
-        map2.put("monster", monster);
+    @After
+    public void tearDown() throws Exception {
+        expectedPage = null;
+        commentService = null;
+        transactionExecutor = null;
+        commentDao = null;
+        userDao = null;
+        content = null;
+        monsterService = null;
+        requestParameters = null;
+        sessionAttributes = null;
+        user = null;
+        monster = null;
+    }
 
-
-
-        doReturn(new CommandResult(CommandResult.ResponseType.FORWARD, expectedPage)).when(monsterService).pickMonster(content);
-        when(content.getRequestParameters()).thenReturn(map1);
-        when(content.getSessionAttributes()).thenReturn(map2);
+    @Test
+    public void addComment() throws Exception {
+        when(content.getRequestParameters()).thenReturn(requestParameters);
+        when(content.getSessionAttributes()).thenReturn(sessionAttributes);
         when(CommentValidator.addCommentValidator(anyString(), anyString())).thenReturn(true);
         when(UserValidation.hasRoleAdminOrUser(any())).thenReturn(true);
-        commentService.addComment(content);
-
+        doReturn(new CommandResult(CommandResult.ResponseType.FORWARD, expectedPage)).when(monsterService).pickMonster(content);
         PowerMockito.whenNew(RequestContent.class).withAnyArguments().thenReturn(content);
         PowerMockito.whenNew(CommentDao.class).withAnyArguments().thenReturn(commentDao);
         PowerMockito.whenNew(TransactionExecutor.class).withNoArguments().thenReturn(transactionExecutor);
         PowerMockito.whenNew(MonsterService.class).withAnyArguments().thenReturn(monsterService);
         PowerMockito.whenNew(UserDao.class).withAnyArguments().thenReturn(userDao);
-
-        verify(commentService, times(1)).addComment(content);
+        commentService.addComment(content);
         verify(content, times(2)).getRequestParameters();
         verify(content, times(3)).getSessionAttributes();
         assertEquals(CommandResult.ResponseType.FORWARD, commentService.addComment(content).getResponseType());
--- src/test/senchenko/aliens/service/UserServiceTest.java
+++ src/test/senchenko/aliens/service/UserServiceTest.java
@@ -13,36 +13,67 @@ import com.senchenko.aliens.service.MonsterService;
 import com.senchenko.aliens.service.UserService;
 import com.senchenko.aliens.validation.CommentValidator;
 import com.senchenko.aliens.validation.UserValidation;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import java.util.ArrayList;
 import java.util.HashMap;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
-import static org.testng.Assert.assertEquals;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({TransactionExecutor.class, RequestContent.class, CommentValidator.class, UserValidation.class, UserValidation.class,
          SingletonDaoProvider.class, UserDao.class, FrontServlet.class, ConnectionPool.class, UserService.class})
 public class UserServiceTest {
+    HashMap<String, String[]> requestParameters;
+    HashMap<String, Object> sessionAttributes;
+    User user;
+    UserService userService;
+    TransactionExecutor transactionExecutor;
+    UserDao userDao;
+    MonsterService monsterService;
+    RequestContent content;
+
+    @Before
+    public void setUp() throws Exception {
+        requestParameters = new HashMap<>();
+        sessionAttributes = new HashMap<>();
+        requestParameters.put("login", new String[]{"test"});
+        requestParameters.put("password", new String[]{"pass"});
+        requestParameters.put("email", new String[]{"email"});
+        user = new User(1, new Role(1, "admin"),
+                1, "test", "test", "test");
+        sessionAttributes.put("user",user);
+        PowerMockito.mockStatic(UserValidation.class);
+        userService = new UserService();
+        transactionExecutor = mock(TransactionExecutor.class);
+        userDao = mock(UserDao.class);
+        monsterService = mock(MonsterService.class);
+        content = mock(RequestContent.class);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        user = null;
+        requestParameters = null;
+        sessionAttributes = null;
+    }
 
     @Test
     public void goToLoginPage() {
-        UserService userService =  spy(UserService.class);
-        RequestContent content = mock(RequestContent.class);
         String expectedPage = "/jsp/login.jsp";
         assertEquals(CommandResult.ResponseType.FORWARD, userService.goToLoginPage(content).getResponseType());
         assertEquals(expectedPage, userService.goToLoginPage(content).getPage());
-
     }
 
     @Test
     public void goToRegistrationPage() {
-        UserService userService =  spy(UserService.class);
-        RequestContent content = mock(RequestContent.class);
         String expectedPage = "/jsp/registration.jsp";
         assertEquals(CommandResult.ResponseType.FORWARD, userService.goToRegistrationPage(content).getResponseType());
         assertEquals(expectedPage, userService.goToRegistrationPage(content).getPage());
@@ -51,54 +82,60 @@ public class UserServiceTest {
     @Test
     public void login() throws Exception {
         String expectedPage = "/jsp/monsters.jsp";
-        UserService userService = new UserService();
-        TransactionExecutor transactionExecutor = mock(TransactionExecutor.class);
-
-        PowerMockito.mockStatic(UserValidation.class);
-        UserDao userDao = mock(UserDao.class);
-        MonsterService monsterService = mock(MonsterService.class);
-        RequestContent content = mock(RequestContent.class);
-
-        HashMap<String, String[]> map1 = new HashMap<>();
-        HashMap<String, Object> map2 = new HashMap<>();
-        map1.put("login", new String[]{"test"});
-        map1.put("password", new String[]{"pass"});
-        User user = new User(1, new Role(1, "admin"), 1, "test", "test", "test");
-        map2.put("user",user);
-
-        when(monsterService.goToMonstersPage(content)).thenReturn(new CommandResult(CommandResult.ResponseType.FORWARD,expectedPage));
+        when(monsterService.goToMonstersPage(content))
+                .thenReturn(new CommandResult(CommandResult.ResponseType.FORWARD,expectedPage));
         when(userDao.findUserByLogin(anyString())).thenReturn(user);
-        when(content.getRequestParameters()).thenReturn(map1);
+        when(content.getRequestParameters()).thenReturn(requestParameters);
         when(UserValidation.loginValidator(any(), any())).thenReturn(true);
-
         PowerMockito.whenNew(RequestContent.class).withAnyArguments().thenReturn(content);
         PowerMockito.whenNew(TransactionExecutor.class).withAnyArguments().thenReturn(transactionExecutor);
         PowerMockito.whenNew(UserDao.class).withAnyArguments().thenReturn(userDao);
         PowerMockito.whenNew(MonsterService.class).withAnyArguments().thenReturn(monsterService);
-
         assertEquals(CommandResult.ResponseType.FORWARD, userService.login(content).getResponseType());
         assertEquals(expectedPage, userService.login(content).getPage());
-
     }
 
     @Test
     public void logout() {
-        UserService userService =  spy(UserService.class);
-        RequestContent content = mock(RequestContent.class);
         String expectedPage = "/jsp/login.jsp";
         assertEquals(CommandResult.ResponseType.INVALIDATE, userService.logout(content).getResponseType());
         assertEquals(expectedPage, userService.logout(content).getPage());
     }
 
     @Test
-    public void register() {
+    public void register() throws Exception {
+        String expectedPage = "/jsp/login.jsp";
+        when(content.getRequestParameters()).thenReturn(requestParameters);
+        when(UserValidation.registrationValidator(any(), any(), any())).thenReturn(true);
+        PowerMockito.whenNew(UserDao.class).withAnyArguments().thenReturn(userDao);
+        PowerMockito.whenNew(TransactionExecutor.class).withAnyArguments().thenReturn(transactionExecutor);
+        assertEquals(CommandResult.ResponseType.FORWARD, userService.login(content).getResponseType());
+        assertEquals(expectedPage, userService.login(content).getPage());
     }
 
     @Test
-    public void changeRole() {
+    public void changeRole() throws Exception {
+        String expectedPage = "/jsp/users.jsp";
+        when(content.getRequestParameters()).thenReturn(requestParameters);
+        when(content.getSessionAttributes()).thenReturn(sessionAttributes);
+        when(userDao.findById(anyInt())).thenReturn(user);
+        when(UserValidation.hasRoleAdminOrUser(any())).thenReturn(true);
+        when(UserValidation.hasRoleAdmin(any())).thenReturn(true);
+        PowerMockito.whenNew(UserDao.class).withAnyArguments().thenReturn(userDao);
+        PowerMockito.whenNew(TransactionExecutor.class).withAnyArguments().thenReturn(transactionExecutor);
+        assertEquals(CommandResult.ResponseType.FORWARD, userService.login(content).getResponseType());
+        assertEquals(expectedPage, userService.goToUsersPage(content).getPage());
     }
 
     @Test
-    public void goToUsersPage() {
+    public void goToUsersPage() throws Exception {
+        String expectedPage = "/jsp/users.jsp";
+        when(content.getSessionAttributes()).thenReturn(sessionAttributes);
+        when(UserValidation.hasRoleAdmin(any())).thenReturn(true);
+        when(userDao.findAll()).thenReturn(new ArrayList<>());
+        PowerMockito.whenNew(UserDao.class).withAnyArguments().thenReturn(userDao);
+        PowerMockito.whenNew(TransactionExecutor.class).withAnyArguments().thenReturn(transactionExecutor);
+        assertEquals(CommandResult.ResponseType.FORWARD, userService.goToUsersPage(content).getResponseType());
+        assertEquals(expectedPage, userService.goToUsersPage(content).getPage());
     }
 }
